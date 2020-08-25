40d6560443aa9f9a65614afb45ea8a253b1d3309
nirajgeorgian <nirajgeorgian@gmail.com>
2020-08-26 02:58:33 +0530

    added option to generate build file for uploading

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MergeIntoSingle = require("webpack-merge-and-include-globally");
+const uglifyJS = require("uglify-js");
+const CleanCSS = require("clean-css");
 const glob = require("glob");
 
 module.exports = {
   mode: "production",
+  target: "web",
   entry: {
-    "bundle.js": glob.sync("build/static/?(js|css)/*.?(js|css)").map((f) => {
-      console.log(f);
-      return path.resolve(__dirname, f);
-    }),
+    "bundle.js": glob
+      .sync("build/static/?(js|css)/*.?(js|css)")
+      .map((f) => path.resolve(__dirname, f)),
   },
   output: {
-    path: path.resolve(__dirname),
-    filename: "build/static/js/bundle.min.js",
+    filename: "build/bundle.js",
   },
   module: {
     rules: [
@@ -22,6 +24,41 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new MergeIntoSingle(
+      {
+        files: [
+          {
+            src: glob
+              .sync("build/static/?(js)/*.?(js)")
+              .map((f) => path.resolve(__dirname, f)),
+            dest: (code) => {
+              const min = uglifyJS.minify(code, {
+                sourceMap: {
+                  filename: "message-box.js",
+                },
+              });
+              return {
+                "message-box.js": min.code,
+              };
+            },
+          },
+          {
+            src: glob
+              .sync("build/static/?(css)/*.?(css)")
+              .map((f) => path.resolve(__dirname, f)),
+            dest: (code) => ({
+              "style.css": new CleanCSS({}).minify(code).styles,
+            }),
+          },
+        ],
+        hash: false,
+      },
+      (filesMap) => {
+        console.log("generated files: ", filesMap);
+      }
+    ),
+  ],
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
